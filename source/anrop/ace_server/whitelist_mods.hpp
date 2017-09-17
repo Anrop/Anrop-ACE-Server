bd754c0c852858bf730fc699788408d836bf9b3f
Björn Dahlgren <bjorn@dahlgren.at>
2017-09-17 19:15:31 +0200

    Whitelist STUI

---
+++ source/anrop/ace_server/whitelist_mods.hpp
@@ -0,0 +1,3 @@
+#define WHITELIST_MOD_ANROP_SERVER "'anrop_aar', 'anrop_ace_server', 'xea_testextension'"
+#define WHITELIST_MOD_STUI "'stui_autobrightness', 'stui_core', 'stui_grouphud', 'stui_groupindicators', 'stui_nametags', 'stui_stamina'"
+#define WHITELIST_MODS __EVAL("[" + WHITELIST_MOD_ANROP_SERVER + ", " + WHITELIST_MOD_STUI + "]")
